7ac0d92a6acb25012a1e5f4385b019cebbbb883c
HyangRim <curomame@naver.com>
2022-04-15 22:57:03 +0900

    backjoon

---
+++ 1.JS/5.backjoon.js
@@ -0,0 +1,14 @@
+//backjoon문제 하나씩 푸는즁
+
+//back joon은 nodejs로 풀어야해서 아래와 같이 작성 후 시작한다
+
+//fs module로 파일 불러옴
+const fs = require ('fs');
+
+//이런식으로 데이터를 불러와야 문제 풀이 시작이 가능해진다
+const data = fs.readFileSync('/dev/stdin').toString().split('xxx');
+
+
+//아주 정신없이 문제풀면서 행복을 느끼는 중이다.
+//세상 재밌는게 없네 이것보다
+
